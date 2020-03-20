07d1680cc701bc807fa89d2b13fc883e6da949e2
Bruno Nascimento <bruno.bmn@gmail.com>
2020-03-19 21:04:33 -0300

    Api url adjust

--- src/app/services/api.js
+++ src/app/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: process.env.REACT_APP_URL_ICM_CORE || 'http://localhost:3333'
+  baseURL: 'https://icm-core.herokuapp.com/'
 });
